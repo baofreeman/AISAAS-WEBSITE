bca7098e37be1a11f8517e7e3d5f52547bb0f8a8
Hoang <freeman.dev10@gmail.com>
2024-09-14 00:25:08 +0700

    final docker

--- app/api/stripe-customer-portal/route.ts
+++ app/api/stripe-customer-portal/route.ts
@@ -15,19 +15,18 @@ export async function POST(req: Request) {
       where: { userId: user.id },
     });
 
+    const dashboardUrl = absoluteUrl("/dashboard");
+
     if (!userSubscription || !userSubscription.stripeCustomerId) {
       return new NextResponse("Subscription not found", { status: 404 });
     }
-    const dashboardUrl = absoluteUrl("/dashboard");
 
-    const portalSession = await stripe.billingPortal.sessions.create({
+    const session = await stripe.billingPortal.sessions.create({
       customer: userSubscription.stripeCustomerId,
       return_url: dashboardUrl,
     });
 
-    return new NextResponse(JSON.stringify({ url: portalSession.url }), {
-      status: 200,
-    });
+    return NextResponse.json({ url: session.url }, { status: 200 });
   } catch (error) {
     console.error("Error creating customer portal session:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
